0c25e0f13fea566b83b95eb694cd2937fc7d3c5d
Kailai Xu <kailaix@hotmail.com>
2020-08-26 01:40:05 -0700

    update bench.sh

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 for n in 2 4 8 16 32 64 128 256
 do 
-mpirun -n $n ./build/bench
+/opt/ohpc/pub/compiler/intel-18/compilers_and_libraries_2018.2.199/linux/mpi/intel64/bin/mpirun -n $n ./build/bench
 done
